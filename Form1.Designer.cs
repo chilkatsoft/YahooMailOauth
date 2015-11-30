7001ab69df3040241dbaae7c9398312dda9c8a59
chilkatsoft <admin@chilkatsoft.com>
2015-11-30 14:57:37 -0600

    Initial Release

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace YahooMailOauth1
+    {
+    partial class Form1
+	{
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	/// Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing)
+	    {
+	    if (disposing && (components != null))
+		{
+		components.Dispose();
+		}
+	    base.Dispose(disposing);
+	    }
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	    {
+	    this.button1 = new System.Windows.Forms.Button();
+	    this.textBox1 = new System.Windows.Forms.TextBox();
+	    this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+	    this.button3 = new System.Windows.Forms.Button();
+	    this.button2 = new System.Windows.Forms.Button();
+	    this.txtVerifier = new System.Windows.Forms.TextBox();
+	    this.label1 = new System.Windows.Forms.Label();
+	    this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+	    ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+	    this.splitContainer1.Panel1.SuspendLayout();
+	    this.splitContainer1.Panel2.SuspendLayout();
+	    this.splitContainer1.SuspendLayout();
+	    this.SuspendLayout();
+	    // 
+	    // button1
+	    // 
+	    this.button1.Location = new System.Drawing.Point(23, 12);
+	    this.button1.Name = "button1";
+	    this.button1.Size = new System.Drawing.Size(196, 25);
+	    this.button1.TabIndex = 0;
+	    this.button1.Text = "Step 1: Get A Request Token";
+	    this.button1.UseVisualStyleBackColor = true;
+	    this.button1.Click += new System.EventHandler(this.button1_Click);
+	    // 
+	    // textBox1
+	    // 
+	    this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+	    this.textBox1.Location = new System.Drawing.Point(0, 248);
+	    this.textBox1.Multiline = true;
+	    this.textBox1.Name = "textBox1";
+	    this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+	    this.textBox1.Size = new System.Drawing.Size(554, 406);
+	    this.textBox1.TabIndex = 1;
+	    // 
+	    // splitContainer1
+	    // 
+	    this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+	    this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+	    this.splitContainer1.Name = "splitContainer1";
+	    // 
+	    // splitContainer1.Panel1
+	    // 
+	    this.splitContainer1.Panel1.Controls.Add(this.button3);
+	    this.splitContainer1.Panel1.Controls.Add(this.button2);
+	    this.splitContainer1.Panel1.Controls.Add(this.txtVerifier);
+	    this.splitContainer1.Panel1.Controls.Add(this.label1);
+	    this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+	    this.splitContainer1.Panel1.Controls.Add(this.button1);
+	    // 
+	    // splitContainer1.Panel2
+	    // 
+	    this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+	    this.splitContainer1.Size = new System.Drawing.Size(1226, 654);
+	    this.splitContainer1.SplitterDistance = 554;
+	    this.splitContainer1.TabIndex = 2;
+	    // 
+	    // button3
+	    // 
+	    this.button3.Location = new System.Drawing.Point(23, 133);
+	    this.button3.Name = "button3";
+	    this.button3.Size = new System.Drawing.Size(196, 47);
+	    this.button3.TabIndex = 5;
+	    this.button3.Text = "Step 4: Use Access Token with IMAP";
+	    this.button3.UseVisualStyleBackColor = true;
+	    this.button3.Click += new System.EventHandler(this.button3_Click);
+	    // 
+	    // button2
+	    // 
+	    this.button2.Location = new System.Drawing.Point(23, 99);
+	    this.button2.Name = "button2";
+	    this.button2.Size = new System.Drawing.Size(196, 28);
+	    this.button2.TabIndex = 4;
+	    this.button2.Text = "Step 3: Get the Access Token";
+	    this.button2.UseVisualStyleBackColor = true;
+	    this.button2.Click += new System.EventHandler(this.button2_Click);
+	    // 
+	    // txtVerifier
+	    // 
+	    this.txtVerifier.Location = new System.Drawing.Point(23, 64);
+	    this.txtVerifier.Name = "txtVerifier";
+	    this.txtVerifier.Size = new System.Drawing.Size(204, 20);
+	    this.txtVerifier.TabIndex = 3;
+	    // 
+	    // label1
+	    // 
+	    this.label1.AutoSize = true;
+	    this.label1.Location = new System.Drawing.Point(22, 48);
+	    this.label1.Name = "label1";
+	    this.label1.Size = new System.Drawing.Size(281, 13);
+	    this.label1.TabIndex = 2;
+	    this.label1.Text = "Step 2: Copy-and-Paste the Verifier from the Browser here:";
+	    // 
+	    // webBrowser1
+	    // 
+	    this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+	    this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+	    this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+	    this.webBrowser1.Name = "webBrowser1";
+	    this.webBrowser1.Size = new System.Drawing.Size(668, 654);
+	    this.webBrowser1.TabIndex = 0;
+	    // 
+	    // Form1
+	    // 
+	    this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+	    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+	    this.ClientSize = new System.Drawing.Size(1226, 654);
+	    this.Controls.Add(this.splitContainer1);
+	    this.Name = "Form1";
+	    this.Text = "Yahoo Mail with OAuth";
+	    this.Load += new System.EventHandler(this.Form1_Load);
+	    this.splitContainer1.Panel1.ResumeLayout(false);
+	    this.splitContainer1.Panel1.PerformLayout();
+	    this.splitContainer1.Panel2.ResumeLayout(false);
+	    ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+	    this.splitContainer1.ResumeLayout(false);
+	    this.ResumeLayout(false);
+
+	    }
+
+	#endregion
+
+	private System.Windows.Forms.Button button1;
+	private System.Windows.Forms.TextBox textBox1;
+	private System.Windows.Forms.SplitContainer splitContainer1;
+	private System.Windows.Forms.WebBrowser webBrowser1;
+	private System.Windows.Forms.Button button2;
+	private System.Windows.Forms.TextBox txtVerifier;
+	private System.Windows.Forms.Label label1;
+	private System.Windows.Forms.Button button3;
+	}
+    }
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YahooMailOauth1
+    {
+    public partial class Form1 : Form
+	{
+	// Create your YDN app at https://developer.yahoo.com/apps/create/
+	// Make sure to give full access to the Mail API.
+
+	//Client ID (Consumer Key)
+	private string ConsumerKey = "Set this to your base64 consumer key.";
+
+	//Client Secret (Consumer Secret)
+	private string ConsumerSecret = "Set this to your hex consumer secret.";
+
+	private string ChilkatUnlockCode = "Your Chilkat unlock code or anything for an automatic 30-day trial.";
+
+	private string YahooMailPassword = "Your Yahoo Mail Password";
+	private string YahooMailUsername = "Your Yahoo Mail username/email address";
+
+
+	private string OAuthToken = "To be generated in Step1";
+	private string OAuthTokenSecret = "To be generated in Step1";
+
+	private string OAuthAccessToken = "To be generated in Step3";
+	private string OAuthAccessTokenSecret = "To be generated in Step3";
+
+
+	public Form1()
+	    {
+	    InitializeComponent();
+	    }
+
+	private void Form1_Load(object sender, EventArgs e)
+	    {
+	    // Unlock the Chilkat functionality.  
+	    // This is an automatic 30-day trial, but requires a purchased license to use indefinitely.
+	    Chilkat.Global global = new Chilkat.Global();
+	    bool success = global.UnlockBundle(ChilkatUnlockCode);
+	    if (!success)
+		{
+		textBox1.Text = "Failed to unlock Chilkat.";
+		return;
+		}
+	    }
+
+
+	// Get the request token and verifier.
+	private void button1_Click(object sender, EventArgs e)
+	    {
+	    Chilkat.OAuth1 oauth = new Chilkat.OAuth1();
+
+	    oauth.GenNonce(16);
+	    oauth.GenTimestamp();
+
+	    // The 1st step is to get a request token by sending an HTTP GET like this:
+	    oauth.OauthVersion = "1.0";
+	    oauth.OauthMethod = "GET";
+	    oauth.OauthUrl = "https://api.login.yahoo.com/oauth/v2/get_request_token";
+	    oauth.ConsumerKey = ConsumerKey;
+	    oauth.ConsumerSecret = ConsumerSecret;
+	    oauth.SignatureMethod = "HMAC-SHA1";
+	    oauth.AddParam("oauth_callback", "oob");
+
+	    // Generate the OAuth1 signature and URL.
+	    bool success = oauth.Generate();
+	    if (success != true) {
+		textBox1.Text = oauth.LastErrorText;
+		return;
+	    }
+
+	    // Properties set by the Generate method:
+	    //textBox1.Text = "BaseString: " + oauth.BaseString + "\r\n" +
+	    //    "EncodedSignature : " + oauth.EncodedSignature + "\r\n" +
+	    //    "HmacKey : " + oauth.HmacKey + "\r\n" +
+	    //    "GeneratedUrl : " + oauth.GeneratedUrl + "\r\n" +
+	    //    "QueryString : " + oauth.QueryString + "\r\n" +
+	    //    "Signature  : " + oauth.Signature + "\r\n" +
+	    //    "AuthorizationHeader  : " + oauth.AuthorizationHeader + "\r\n"
+	    //    ;
+
+	    string requestTokenUrl = oauth.GeneratedUrl + "&oauth_signature=" + oauth.EncodedSignature;
+	    
+	    Chilkat.Http http = new Chilkat.Http();
+	    
+	    // Get the request token...
+	    Chilkat.HttpResponse resp = http.QuickGetObj(requestTokenUrl);
+	    if (resp == null) {
+		textBox1.Text = http.LastErrorText;
+		return;
+		}
+
+	    // Get User Authorization -- get a request verifier interactively via the embedded web browser:
+	    string encodedResponseParams = resp.BodyStr;
+	    OAuthToken = resp.UrlEncParamValue(encodedResponseParams, "oauth_token");
+	    OAuthTokenSecret = resp.UrlEncParamValue(encodedResponseParams, "oauth_token_secret");
+	    string userAuthUrl = "https://api.login.yahoo.com/oauth/v2/request_auth?oauth_token=" + OAuthToken;
+
+	    webBrowser1.Navigate(userAuthUrl);
+
+	    return;
+	    }
+
+	// Exchange the Request Token and OAuth Verifier for an Access Token
+	private void button2_Click(object sender, EventArgs e)
+	    {
+	    Chilkat.OAuth1 oauth = new Chilkat.OAuth1();
+
+	    oauth.GenNonce(16);
+	    oauth.GenTimestamp();
+
+	    // The 1st step is to get a request token by sending an HTTP GET like this:
+	    oauth.OauthVersion = "1.0";
+	    oauth.OauthMethod = "GET";
+	    oauth.OauthUrl = "https://api.login.yahoo.com/oauth/v2/get_token";
+	    oauth.ConsumerKey = ConsumerKey;
+	    oauth.ConsumerSecret = ConsumerSecret;
+	    oauth.Token = OAuthToken;
+	    oauth.TokenSecret = OAuthTokenSecret;
+	    oauth.SignatureMethod = "HMAC-SHA1";
+	    oauth.AddParam("oauth_verifier", txtVerifier.Text);
+
+	    // Generate the OAuth1 signature and URL.
+	    bool success = oauth.Generate();
+	    if (success != true)
+		{
+		textBox1.Text = oauth.LastErrorText;
+		return;
+		}
+
+	    string getAccessTokenUrl = oauth.GeneratedUrl + "&oauth_signature=" + oauth.EncodedSignature;
+
+	    Chilkat.Http http = new Chilkat.Http();
+
+	    // Get the request token...
+	    Chilkat.HttpResponse resp = http.QuickGetObj(getAccessTokenUrl);
+	    if (resp == null)
+		{
+		textBox1.Text = http.LastErrorText;
+		return;
+		}
+
+	    // Get the access token.  
+	    string encodedResponseParams = resp.BodyStr;
+	    OAuthAccessToken = resp.UrlEncParamValue(encodedResponseParams, "oauth_token");
+	    OAuthAccessTokenSecret = resp.UrlEncParamValue(encodedResponseParams, "oauth_token_secret");
+
+	    textBox1.Text = "success!\n";
+
+	    return;
+	    }
+
+	// Step 4: Can we use the Access Token with IMAP?
+	// This does not work.  We don't know how to use an OAuth1 access token
+	// with the IMAP protocol.  We don't know how to obtain (via Yahoo) an OAuth2
+	// access token for the IMAP XOAUTH2 authentication method.
+	private void button3_Click(object sender, EventArgs e)
+	    {
+	    Chilkat.Imap imap = new Chilkat.Imap();
+
+	    imap.KeepSessionLog = true;
+
+	    //  Connect to they Yahoo! IMAP server.
+	    imap.Port = 993;
+	    imap.Ssl = true;
+	    bool success = imap.Connect("imap.mail.yahoo.com");
+	    if (success != true)
+		{
+		textBox1.Text += "\r\n" + imap.LastErrorText;
+		textBox1.Text += "\r\n" + imap.SessionLog;
+		return;
+		}
+
+	    //  Send the non-standard ID command...
+	    string rawResponse;
+	    rawResponse = imap.SendRawCommand("ID (\"GUID\" \"1\")");
+	    if (rawResponse == null)
+		{
+		textBox1.Text += "\r\n" + imap.LastErrorText;
+		textBox1.Text += "\r\n" + imap.SessionLog;
+		return;
+		}
+
+	    //rawResponse = imap.SendRawCommand("AUTHENTICATE XOAUTH2 " + OAuthAccessToken);
+	    rawResponse = imap.SendRawCommand("AUTHENTICATE OAUTHBEARER " + OAuthAccessToken);
+	    if (rawResponse == null)
+		{
+		textBox1.Text += "\r\n" + imap.LastErrorText;
+		textBox1.Text += "\r\n" + imap.SessionLog;
+		return;
+		}
+	    textBox1.Text += "\r\n" + imap.LastErrorText;
+	    textBox1.Text += "\r\n" + imap.SessionLog;
+
+	    //  Login
+	    //success = imap.Login(YahooMailUsername, YahooMailPassword);
+	    //if (success != true)
+	    //    {
+	    //    textBox1.Text = imap.LastErrorText;
+	    //    return;
+	    //    }
+
+	    //  Select an IMAP mailbox
+	    success = imap.SelectMailbox("Inbox");
+	    if (success != true)
+		{
+		textBox1.Text += "\r\n" + imap.LastErrorText;
+		return;
+		}
+
+	    //  Continue with whatever you wish to do...
+	    //  (see other examples..)
+
+	    //  Disconnect from the IMAP server.
+	    imap.Disconnect();
+
+	    textBox1.Text += "\r\nLogin Success!";
+
+	    }
+	}
+    }
